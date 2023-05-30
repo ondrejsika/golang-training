254bf312131ce561d442c3a7548d29ba890fbd91
Ondrej Sika <ondrej@ondrejsika.com>
2023-05-30 11:19:37 +0200

    feat(examples/init): Add example of init function

---
+++ examples/init/hello/hello.go
@@ -0,0 +1,11 @@
+package hello
+
+import "fmt"
+
+func init() {
+	fmt.Println("AD: Support the developers on Github Sponsors!")
+}
+
+func Hello() {
+	fmt.Println("Hello World!")
+}
---
+++ examples/init/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"init/hello"
+)
+
+func main() {
+	hello.Hello()
+}
